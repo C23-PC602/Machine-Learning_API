341c399073957b937796fb50c07228975cd6edab
zamzamnurahman <kazamnn1912@gmail.com>
2023-05-31 19:58:44 +0700

    decode prediction with max probability

--- application/prediction/serve_model.py
+++ application/prediction/serve_model.py
@@ -1,28 +1,38 @@
+
 from io import BytesIO
 import tensorflow as tf
 import numpy as np
+from keras.applications.imagenet_utils import decode_predictions
 
 from PIL import Image
 
-model = None
-
-
-def load_model():
-    news_model = tf.saved_model.load("model.h5")
-    print(news_model)
-    return news_model
-
 
 def predict(image: Image.Image):
-    global model
-    if model is None:
-        model = load_model()
-    image = np.asarray(image.resize((224, 224)))[..., :3]
+    model = tf.keras.models.load_model("application/prediction/model.h5")
+    image = np.asarray(image.resize((224,224)))[..., :3]
+    image = np.expand_dims(image, 0)
+    print(f"IMAGE : {image.shape}")
     image = image / 127.5 - 1.0
-    result = tf.keras.applications.imagenet_utils.decode_predictions(model.predict(image), 2)[0]
-    print(result)
+    prediction = model.predict(image)
+    print(f"result prediction : {prediction[0]}")
+    max_probability = max(prediction[0])
+    max_index = 0
+    for i in range(len(prediction[0])):
+        if prediction[0][i] == max_probability:
+            max_index = i
+
+    print("Probabilitas tertinggi:", max_probability)
+    print("Indeks probabilitas tertinggi:", max_index)
+    if max_index == 0:
+        result = "Matang"
+    elif max_index == 1:
+        result = "Mentah"
+    else:
+        result = "Setengah Matang"
+    print(f"Result :  {result}")
+    return result
 
 
-def read_image(file) -> Image.Image:
+async def read_image(file) -> Image.Image:
     image = Image.open(BytesIO(file))
     return image
--- main.py
+++ main.py
@@ -1,22 +1,26 @@
-import numpy as np
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Response
+
 from application.prediction.serve_model import predict, read_image
-import tensorflow as tf
 
-app: FastAPI = FastAPI()
+app: FastAPI = FastAPI(title='API Model Coffee Detection',
+                       description='This is API for Machine Learning',
+                       version='0.0.1'
+                       )
 
 
-@app.post('/api/predict/image')
-async def predict_image(file: UploadFile = File(...)):
+@app.post('/api/predict')
+async def predict_image(file: UploadFile, response: Response):
     extension = file.filename.split(".")[-1] in ("jpg", "jpeg", "png")
     if not extension:
         return "Format gambar tidak sesuai"
-    prediction = predict(read_image(file))
     try:
-        print("predictions")
+        content = file.file.read()
+        prediction = predict(await read_image(content))
         return {
-            "result": prediction,
+            "result": str(prediction)
         }
-    except:
-        print("error")
-
+    except Exception as error:
+        response.status_code = 500
+        return {
+            "message": str(error)
+        }
